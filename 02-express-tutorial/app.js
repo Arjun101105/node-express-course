a77dde6920fbbc8ccf96c6eb0b0c1b9caa10e7e8
Arjun101105 <arjunnair101105@gmail.com>
2024-08-07 22:44:06 +0530

    update

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,29 @@
-const http = require('http')
-const server = http.createServer((req, res)=>{
-    // console.log(req.method);
-    // console.log(req.url);
-    const url = req.url;
-    // home page
-    if (url==='/') {
-        
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<h1>Home Page</h1>')
-        res.end()
-        //about page
-    }else if (url === '/about') {
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<h1>About Page</h1>')
-        res.end()
-    }
-    // 404
-    else{
-        res.writeHead(404, {'content-type': 'text/html'})
-        res.write('<h1>Page not found ! -_- !</h1>')
-        res.end()
-    }
+const express = require('express')
+const app = express()
 
+let {people} = require('./data')
+
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({extended: false}))
+
+
+app.get('/api/people', (req, res)=>{
+    res.status(200).json({success:true,data:people})
 })
 
-server.listen(5000)
+app.post('/login', (req, res)=>{
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send(`Please provide credentials`)
+    res.send('POST')
+})
+
+
+app.listen(5000,()=>{
+    console.log(`server is listening on port 5000...`);
+    
+})
